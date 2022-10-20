b1b0810b6cfab3286c54cc21394bed50b3ec0b34
Kyle Robert van Winkoop <kylevanwinkoop@gmail.com>
2022-10-19 18:11:58 -0700

    Initial Push Stuff

---
+++ app.js
@@ -0,0 +1,29 @@
+//Got the packages (in the form of objects)
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+var fs = require('fs');
+
+//Send the user the 'index.html' file, to let them interact
+app.get('/',function(req, res){
+	res.sendFile(__dirname + '/client/index.html');
+});
+
+//If its the mirror, send the mirror file.
+app.get('/mirror',function(req, res){
+	res.sendFile(__dirname + '/client/mirrorIndex.html');
+});
+
+app.use('/client', express.static(__dirname + '/client'));
+
+
+//Start the server on a port
+serv.listen(8012);
+console.log("Server started.");
+
+//A list to track all of the sockets
+var SOCKET_LIST = {};
+
+//Creating the io object (to help us talk with the sockets)
+var io = require('socket.io')(serv,{});
+
