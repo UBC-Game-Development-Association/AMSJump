633f8927aa82c0f0d2c443470ab287331ea1b5cd
Kyle Robert van Winkoop <kylevanwinkoop@gmail.com>
2022-10-19 18:47:33 -0700

    Code for response and emit

--- app.js
+++ app.js
@@ -27,3 +27,39 @@ var SOCKET_LIST = {};
 //Creating the io object (to help us talk with the sockets)
 var io = require('socket.io')(serv,{});
 
+//Setting up what to do when someone connects
+io.sockets.on('connection', function(socket){
+    
+    //Give it an ID and add it to the list
+	//console.log('socket connection');
+	socket.id = Math.random();
+	SOCKET_LIST[socket.id] = socket;
+    
+    //Determine what to do when it disconnects
+    socket.on('disconnect', function(){
+        
+		delete SOCKET_LIST[socket.id];
+	});
+	
+	socket.on('test', function(data){
+       console.log(data.message);
+	   socket.emit('response', {});
+    });
+    
+});
+
+
+	//Every so often, update the board
+	setInterval(function(){
+	
+	
+		for(var i in SOCKET_LIST){
+		
+        
+			var socket = SOCKET_LIST[i];
+		
+			socket.emit('response', {});
+		}
+	}, 1000/65);
+
+
