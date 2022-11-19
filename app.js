4a40e30a060f49f08e10a2d6cec446d21d07c2ab
Kyle Robert van Winkoop <kylevanwinkoop@gmail.com>
2022-11-18 18:46:23 -0800

    Added dif player moving

--- app.js
+++ app.js
@@ -35,11 +35,14 @@ io.sockets.on('connection', function(socket){
 	socket.id = Math.random();
 	SOCKET_LIST[socket.id] = socket;
     
+	
+	Player.onConnect(socket);
+	
 	console.log("Connected");
 	
     //Determine what to do when it disconnects
     socket.on('disconnect', function(){
-        
+        delete Player.list[socket.id];
 		delete SOCKET_LIST[socket.id];
 	});
 	
@@ -48,35 +51,122 @@ io.sockets.on('connection', function(socket){
 	   socket.emit('response', {});
     });
 	
-	socket.on('jumpStart', function(data){
-		console.log(data.direction);
-	});
-
-	socket.on('jumpStop', function(data){
-		console.log(data.direction);
-		for(var i in SOCKET_LIST){
-		
-        
-			var sock = SOCKET_LIST[i];
-		
-			sock.emit('update', {data});
-		}
-	});
     
 });
 
+/*
+* Create a new object to hold all of the game state data.
+*/
+var Board = function(){
+	var self = {};
+	
+}
 
-	//Every so often, update the board
-	setInterval(function(){
+/*
+* Lets update the board (ie make it go up, track its state)
+* Returns a pack with game info.
+*/
+Board.Update = function(){
 	
+}
+
+
+/*
+@TODO: part of the board
+var Map = function(){
 	
-		for(var i in SOCKET_LIST){
+}
+*/
+
+/*
+* Create an object to hold player data.
+*/
+var Player = function(id, username, start){
+	
+	var self = {
+		xPos: start.x,
+		yPos: start.y,
+		speedX: 0,
+		speedY: 0,
+	};
+
+	self.playerMove = function(dir){
+		if(dir == 'right'){
+			self.speedX += 0.5;
+		}
+		else{
+			self.speedX += -0.5;
+		}
+	}
+
+	self.updatePlayer = function(){
+		if(self.yPos > 0){
+			self.speedY = self.speedY - 0.5;
+		}else{
+			self.speedY = 0;
+		}
+		
+		//console.log(self.yPos);
+		
+		self.xPos += self.speedX;
+		self.yPos += self.speedY;
+	
+		var selfPack = {
+			x:self.xPos,
+			y:self.yPos,
+		}
+		return selfPack;
+	}
+	
+	Player.list[id] = self;
+	
+	return self;
+}
+
+Player.list = {};
+
+
+/*
+* Update all players here
+* Returns a pack of all player info.
+*/
+Player.Update = function(){
+	var playerPack = [];
+	
+	for(var i in Player.list){
+		playerPack.push(Player.list[i].updatePlayer());
+	}
+	
+	return playerPack;
+}
+
+Player.onConnect = function(socket){
+	var player = Player(socket.id, "No Name", {x:10, y:200}); 
+	
+	socket.on('jumpStart', function(data){
+		
+	});
+
+	socket.on('jumpStop', function(data){
+		player.playerMove(data.direction);
+	});
+}
+
+//Every so often, update the board
+setInterval(function(){
+//We need to store the player data.
+	var pack = {
+		players: Player.Update(),
+	};
+	
+	for(var i in SOCKET_LIST){
 		
         
-			var socket = SOCKET_LIST[i];
+		var socket = SOCKET_LIST[i];
 		
-			//socket.emit('response', {});
-		}
-	}, 1000/65);
+		//Create an update function that gives us an update pack, then use that pack.
+		socket.emit("update", pack);
+	}
+}, 1000/65);
 
 
