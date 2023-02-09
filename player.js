1fea5d0c86b17c2af5e5c8c8aeaf4b04c70835e0
Kyle Robert van Winkoop <kylevanwinkoop@gmail.com>
2023-02-08 18:30:09 -0800

    Fixed syntax bug + direction bug

--- player.js
+++ player.js
@@ -13,7 +13,7 @@ module.exports = class Player{
     releaseJump (dir) {
         if(this.yPos === 0){
             this.speedX += this.readyR;
-            this.speedX += this.readyL;
+            this.speedX -= this.readyL;
             this.speedY = 2*(this.readyL + this.readyR);
             this.readyL = 0;
             this.readyR = 0;
@@ -21,10 +21,10 @@ module.exports = class Player{
     }
 
     updatePlayer() {
-        if((this.readyL>0) && (self.readyL<8.5)){
+        if((this.readyL>0) && (this.readyL<8.5)){
             this.readyL = this.readyL + (0.6/(this.readyL*2));
         }
-		if((this.readyR>0) && (self.readyR<8.5)){
+		if((this.readyR>0) && (this.readyR<8.5)){
             this.readyR = this.readyR + (0.6/(this.readyR*2));
         }
 
